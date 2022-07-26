1aa602eef030cf70380c5fcc2092515b1bcf76b8
Srishailam.Ch <61355197+sree2290@users.noreply.github.com>
2022-07-26 21:23:11 +0530

    Delete Running Sum of 1d Array.py

--- Running Sum of 1d Array.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        new = [0] * len(nums)
-        new[0] = nums[0]
-        for i in range(1,len(nums)):
-            new[i] = new[i-1] + nums[i]
-        return new
